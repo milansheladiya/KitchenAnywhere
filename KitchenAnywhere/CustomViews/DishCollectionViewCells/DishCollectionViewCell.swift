e5cf705549796f294a24a56546750515b4cea8e4
Dishant <dishant0170@gmail.com>
2022-04-20 13:51:39 -0400

    add quantity increment and decrement btn

--- KitchenAnywhere/CustomViews/DishCollectionViewCells/DishCollectionViewCell.swift
+++ KitchenAnywhere/CustomViews/DishCollectionViewCells/DishCollectionViewCell.swift
@@ -15,6 +15,9 @@ class DishCollectionViewCell: UICollectionViewCell {
     @IBOutlet weak var typeLabel: UILabel!
     @IBOutlet weak var descriptionLabel: UILabel!
     
+    @IBOutlet weak var quantityLabel: UILabel!
+    @IBOutlet weak var incrementBtn: UIButton!
+    @IBOutlet weak var decrementBtn: UIButton!
     func setUp(dish: Dish){
         titleLable.text = dish.title
         dishImageView.kf.setImage(with: dish.image?.asUrl )
